5522050312441d769a4811b42158487aa35a5df0
lacimarsik <laci.marsik@gmail.com>
2016-06-20 11:49:48 +0200

    Integrate UI into IntelliJ IDEA

---
+++ src/harmanal/HarmonyAnalyser.java
@@ -0,0 +1,25 @@
+package harmanal;
+
+import javax.swing.*;
+
+public class HarmonyAnalyser {
+	private JTabbedPane tabbedPane1;
+	private JButton Select;
+	private JTextPane textPane1;
+	private JButton captureButton;
+	private JButton captureButton1;
+	private JTextField textField1;
+	private JTextField textField2;
+	private JTextField textField3;
+	private JTextField textField4;
+	private JTextField textField5;
+	private JTextField textField6;
+	private JButton playButton;
+	private JButton playButton1;
+	private JTextPane textPane2;
+	private JTextField textField7;
+	private JButton loadPluginsButton;
+	private JTextPane textPane3;
+	private JTextField textField8;
+	private JButton extractChromasButton;
+}
