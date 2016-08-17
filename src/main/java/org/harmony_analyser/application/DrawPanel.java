c1e05d5b3a06e4cdbbec2a20eec6d67dd0eaf816
lacimarsik <laci.marsik@gmail.com>
2016-08-17 13:42:47 +0200

    Adjust DrawPanels

--- src/main/java/org/harmony_analyser/application/DrawPanel.java
+++ src/main/java/org/harmony_analyser/application/DrawPanel.java
@@ -9,14 +9,7 @@ class DrawPanel extends JPanel {
 		setBorder(BorderFactory.createLineBorder(Color.black));
 	}
 
-	public Dimension getPreferredSize() {
-		return new Dimension(250,200);
-	}
-
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
-		// Draw Text
-		g.drawString("This is my custom Panel!",10,20);
 	}
 }
