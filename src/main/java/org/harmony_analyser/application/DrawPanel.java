0c1dd71bc8453d8a08d26e8fc2ee21de0468ab04
lacimarsik <laci.marsik@gmail.com>
2016-08-19 13:30:26 +0200

    Simple segment drawing

--- src/main/java/org/harmony_analyser/application/DrawPanel.java
+++ src/main/java/org/harmony_analyser/application/DrawPanel.java
@@ -4,12 +4,38 @@ import javax.swing.*;
 import java.awt.*;
 
 class DrawPanel extends JPanel {
+	private Point cursor; // cursor of drawing (moves from left to right on the canvas)
+
+	/* Public / Package methods */
 
 	public DrawPanel() {
 		setBorder(BorderFactory.createLineBorder(Color.black));
+		cursor = new Point();
+		cursor.setLocation(0, 0);
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		g.setColor(Color.RED);
+		drawSegment(g, 0.2, Color.CYAN);
+		drawSegment(g, 0.2, Color.RED);
+		drawSegment(g, 0.2, Color.YELLOW);
+		drawSegment(g, 0.2, Color.BLUE);
+		drawSegment(g, 0.2, Color.BLACK);
+	}
+
+	/* Private methods */
+
+	/**
+	 * Draws segment of relative length from the client length, using the moving cursor
+	 * @param g [Graphics] main Graphics object
+	 * @param length [length] relative length, e.g. 0.2 for 20%-filled segment
+	 */
+	private void drawSegment(Graphics g, double length, Color color) {
+		int widthInPixels = (int) ((double) this.getWidth() * length);
+
+		g.setColor(color);
+		g.fillRect((int) cursor.getX(), (int) cursor.getY(), widthInPixels, this.getHeight());
+		cursor.move((int) cursor.getX() + widthInPixels, (int) cursor.getY());
 	}
 }
