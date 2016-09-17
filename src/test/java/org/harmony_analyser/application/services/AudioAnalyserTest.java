8f9479ad3db103b179064c32c9ef64dcd45a974b
lacimarsik <laci.marsik@gmail.com>
2016-09-17 23:05:32 +0200

    :white_check_mark: Correct test for AudioAnalyser using mocks

--- src/test/java/org/harmony_analyser/application/services/AudioAnalyserTest.java
+++ src/test/java/org/harmony_analyser/application/services/AudioAnalyserTest.java
@@ -1,32 +1,51 @@
 package org.harmony_analyser.application.services;
 
 import org.harmony_analyser.chromanal.Chroma;
-import org.harmony_analyser.plugins.AnalysisPlugin;
+import org.harmony_analyser.plugins.*;
 import org.junit.*;
+import static org.mockito.Mockito.*;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for AudioAnalyser class
  */
 
+@SuppressWarnings("ConstantConditions")
+
 public class AudioAnalyserTest {
 	private AudioAnalyser audioAnalyser;
-	private File testWavFile, testReportFixture;
-	private String inputFile;
+	private String wrongInputFile;
+	private File testWavFile;
 	private String resultFile;
 
-	// XXX: Until Mockito is added as dependency and mocks are in practice, expect the fail scenario rather than proper call
-
 	@Before
-	public void setUp() {
-		audioAnalyser = new AudioAnalyser();
-		inputFile = "wrongfile";
+	public void setUp() throws Exception {
+		wrongInputFile = "wrongfile";
+		ClassLoader classLoader = getClass().getClassLoader();
+		testWavFile = new File(classLoader.getResource("test.wav").getPath());
 	}
 
 	@Test(expected = AnalysisPlugin.IncorrectInputException.class)
 	public void shouldThrowExceptionOnWrongFile() throws IOException, AudioAnalyser.LoadFailedException, AnalysisPlugin.IncorrectInputException, AnalysisPlugin.OutputAlreadyExists, Chroma.WrongChromaSize {
-		audioAnalyser.runAnalysis(inputFile, "harmanal:transition_complexity", true);
+		AnalysisPluginFactory analysisPluginFactory = new AnalysisPluginFactory();
+		audioAnalyser = new AudioAnalyser(analysisPluginFactory);
+		audioAnalyser.runAnalysis(wrongInputFile.toString(), "harmanal:transition_complexity", true);
+	}
+
+	@Test
+	public void shouldCallPluginAnalyse() throws IOException, AudioAnalyser.LoadFailedException, AnalysisPlugin.IncorrectInputException, AnalysisPlugin.OutputAlreadyExists, Chroma.WrongChromaSize {
+		AnalysisPlugin analysisPlugin = mock(AnalysisPlugin.class);
+		when(analysisPlugin.analyse(testWavFile.toString(), true)).thenReturn("Done!");
+
+		AnalysisPluginFactory analysisPluginFactory = new AnalysisPluginFactory() {
+			public AnalysisPlugin createPlugin(String pluginKey) {
+				return analysisPlugin;
+			}
+		};
+		audioAnalyser = new AudioAnalyser(analysisPluginFactory);
+
+		assertEquals("Done!", audioAnalyser.runAnalysis(testWavFile.toString(), "harmanal:transition_complexity", true));
 	}
 }
