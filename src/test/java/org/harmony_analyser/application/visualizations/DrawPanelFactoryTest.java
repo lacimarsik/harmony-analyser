017822922f9dbeb308b017f2b60e3a958d3838b7
lacimarsik <laci.marsik@gmail.com>
2016-09-26 19:18:25 +0200

    :white_check_mark: Powerful tests for DrawPanelFactory

--- src/test/java/org/harmony_analyser/application/visualizations/DrawPanelFactoryTest.java
+++ src/test/java/org/harmony_analyser/application/visualizations/DrawPanelFactoryTest.java
@@ -1,13 +1,19 @@
 package org.harmony_analyser.application.visualizations;
 
+import org.harmony_analyser.plugins.AnalysisPluginFactory;
 import org.junit.runner.RunWith;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.verifyNew;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 /**
@@ -16,22 +22,43 @@ import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(DrawPanelFactory.class)
+@PowerMockRunnerDelegate(value = Parameterized.class)
 public class DrawPanelFactoryTest {
-	private DrawPanelFactory drawPanelFactory;
+	private String pluginKey;
+	private static DrawPanelFactory drawPanelFactory = new DrawPanelFactory();
+	private AnalysisPluginFactory analysisPluginFactory;
+	private VisualizationData visualizationData;
 
 	@Before
 	public void setUp() {
-		drawPanelFactory = new DrawPanelFactory();
+		analysisPluginFactory = new AnalysisPluginFactory();
+		visualizationData = VisualizationData.VOID_VISUALIZATION_DATA;
+	}
+
+	public DrawPanelFactoryTest(String pluginKey) {
+		this.pluginKey = pluginKey;
+	}
+
+	@Parameterized.Parameters
+	public static Collection plugins() {
+		return Arrays.asList(drawPanelFactory.getAllVisualizations());
 	}
 
 	@Test
 	public void shouldCreateDrawPanel() throws Exception {
-		SegmentationDrawPanel segmentationDrawPanel = mock(SegmentationDrawPanel.class);
-		VisualizationData visualizationData = mock(VisualizationData.class);
-		whenNew(SegmentationDrawPanel.class).withArguments(any(VisualizationData.class)).thenReturn(segmentationDrawPanel);
+		PaletteDrawPanel paletteDrawPanel = mock(PaletteDrawPanel.class);
+		ChromaDrawPanel chromaDrawPanel = mock(ChromaDrawPanel.class);
+		SegmentationDrawPanel segmenatationDrawPanel = mock(SegmentationDrawPanel.class);
+		ComplexityChartDrawPanel complexityChartDrawPanel = mock(ComplexityChartDrawPanel.class);
 
-		DrawPanel drawPanel = drawPanelFactory.createDrawPanel("nnls-chroma:chordino", visualizationData);
+		whenNew(PaletteDrawPanel.class).withArguments(visualizationData).thenReturn(paletteDrawPanel);
+		whenNew(ChromaDrawPanel.class).withArguments(visualizationData, "Simple").thenReturn(chromaDrawPanel);
+		whenNew(ChromaDrawPanel.class).withArguments(visualizationData, "Tonal").thenReturn(chromaDrawPanel);
+		whenNew(SegmentationDrawPanel.class).withArguments(visualizationData).thenReturn(segmenatationDrawPanel);
+		whenNew(ComplexityChartDrawPanel.class).withArguments(visualizationData).thenReturn(complexityChartDrawPanel);
 
-		verifyNew(SegmentationDrawPanel.class).withArguments(any(VisualizationData.class));
+		DrawPanel drawPanelNew = drawPanelFactory.createDrawPanel(pluginKey, visualizationData);
+
+		assertTrue(drawPanelNew != null);
 	}
 }
