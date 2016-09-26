9376869a45be044f43441352e86739d7d28bb753
lacimarsik <laci.marsik@gmail.com>
2016-09-26 17:22:46 +0200

    :white_check_mark: Test for AnalysisPluginFactory

---
+++ src/test/java/org/harmony_analyser/plugins/AnalysisPluginFactoryTest.java
@@ -0,0 +1,37 @@
+package org.harmony_analyser.plugins;
+
+import org.harmony_analyser.plugins.chordanal_plugins.TransitionComplexityPlugin;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.verifyNew;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+/**
+ * Unit tests for AnalysisPluginFactory class
+ */
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(AnalysisPluginFactory.class)
+public class AnalysisPluginFactoryTest {
+	private AnalysisPluginFactory analysisPluginFactory;
+
+	@Before
+	public void setUp() {
+		analysisPluginFactory = new AnalysisPluginFactory();
+	}
+
+	@Test
+	public void shouldCreateAnalysisPlugin() throws Exception {
+		TransitionComplexityPlugin transitionComplexityPlugin = mock(TransitionComplexityPlugin.class);
+		whenNew(TransitionComplexityPlugin.class).withNoArguments().thenReturn(transitionComplexityPlugin);
+
+		AnalysisPlugin analysisPlugin = analysisPluginFactory.createPlugin("harmanal:transition_complexity");
+
+		verifyNew(TransitionComplexityPlugin.class).withNoArguments();
+	}
+}
