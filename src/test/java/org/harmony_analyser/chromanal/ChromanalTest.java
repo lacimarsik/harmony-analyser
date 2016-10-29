5485bdcfc618842b93d4672c474819e26b766076
lacimarsik <laci.marsik@gmail.com>
2016-10-29 13:14:41 +0200

    :white_check_mark: Update specs

--- src/test/java/org/harmony_analyser/chromanal/ChromanalTest.java
+++ src/test/java/org/harmony_analyser/chromanal/ChromanalTest.java
@@ -17,25 +17,26 @@ public class ChromanalTest {
 	@Before
 	public void setUp() throws Chroma.WrongChromaSize {
 		chroma1 = new Chroma(new float[]{ 1f, 0.3f, 0f, 0f, 1f, 0f, 0f, 1f, 0f, 0f, 0f, 0f }); // C E G
-		chroma2 = new Chroma(new float[]{ 1f, 0f, 0.5f, 0f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 0f }); // C D G
+		chroma2 = new Chroma(new float[]{ 1f, 0f, 0.5f, 0f, 0f, 0f, 0f, 0.8f, 0f, 0f, 0f, 0f }); // C D G
 	}
 
 	@Test
 	public void shouldGetChromaComplexitySimple() {
 		// C#: 0.3 => 0
-		// E: 1 => 0
 		// D: 0 => 0.5
-		// Sum of Deltas: 1.8
-		assertEquals(1.8, Chromanal.getChromaComplexitySimple(chroma1, chroma2), 0.01);
+		// E: 1 => 0
+		// G: 1 => 0.8
+		// Sum of Deltas: 2.0
+		assertEquals(2.0, Chromanal.getChromaComplexitySimple(chroma1, chroma2), 0.01);
 	}
 
 	@Test
 	public void shouldGetChromaComplexityTonal() throws Chroma.WrongChromaSize {
-		// IGNORING CHORD TONES C, E, G
+		// COMMON ROOT HARMONY: C, G (ignoring)
 		// C#: 0.3 => 0
-		// IGNORING CHORD TONES C, G
 		// D: 0.5 => 0
-		// Sum of Deltas: 0.8
-		assertEquals(0.8, Chromanal.getChromaComplexityTonal(chroma1, chroma2, false), 0.01);
+		// E: 1 => 0
+		// Sum of Deltas: 1.8
+		assertEquals(1.8, Chromanal.getChromaComplexityTonal(chroma1, chroma2, true), 0.01);
 	}
 }
