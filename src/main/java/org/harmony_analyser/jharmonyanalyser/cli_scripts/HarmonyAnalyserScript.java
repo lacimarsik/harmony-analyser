37c9c4fd29d03dc8d072cf462d850f0fad945ac0
lacimarsik <laci.marsik@gmail.com>
2017-02-11 23:26:54 +0100

    CLI script skeleton in jharmonyanalyser cli_scripts package

---
+++ src/main/java/org/harmony_analyser/jharmonyanalyser/cli_scripts/HarmonyAnalyserScript.java
@@ -0,0 +1,9 @@
+package org.harmony_analyser.jharmonyanalyser.cli_scripts;
+
+/**
+ * Script to perform arbitrary harmony-analyser analysis in the current directory
+ */
+public class HarmonyAnalyserScript {
+	public static void main(String[] args) {
+	}
+}
