cd2b52d2f9d750f2c587f700146c87ac715ebc9f
lacimarsik <laci.marsik@gmail.com>
2017-11-23 15:32:11 +0100

    Clean up ChordVectorsFIlter

--- src/main/java/org/harmony_analyser/jharmonyanalyser/filters/ChordVectorsFilter.java
+++ src/main/java/org/harmony_analyser/jharmonyanalyser/filters/ChordVectorsFilter.java
@@ -36,8 +36,8 @@ public class ChordVectorsFilter extends LineChartPlugin {
 		description = "Converts Chordino output to a time series of 12-dimensional boolean vectors of chords";
 
 		inputFileSuffixes = new ArrayList<>();
-		inputFileSuffixes.add("-chordino-labels");
 		inputFileSuffixes.add("-chordino-tones");
+		inputFileSuffixes.add("-chordino-labels");
 		inputFileSuffixes.add("-key");
 		inputFileExtension = ".txt";
 
@@ -54,17 +54,22 @@ public class ChordVectorsFilter extends LineChartPlugin {
 
 		BufferedWriter out = new BufferedWriter(new FileWriter(outputFile));
 
-		// 0. Pre-process Chordino chord tones output so it contains only 1 timestamp and relative chord tone names
+		// 0. Pre-process Chordino chord tones output so it contains only 1 timestamp and Chord object
 		// prepares:
 		// chordList (list of Chord models)
 		// chordTimestampList (timestamps related to chordList)
-		List<String> preProcessLinesList = Files.readAllLines(new File(inputFiles.get(1)).toPath(), Charset.defaultCharset());
+		List<String> preProcessLinesList = Files.readAllLines(new File(inputFiles.get(0)).toPath(), Charset.defaultCharset());
 		List<Float> preProcessTimestampList = new ArrayList<>();
 		preProcessTimestampList.addAll(preProcessLinesList.stream().map(AudioAnalysisHelper::getTimestampFromLineContainingTimestampAndLength).collect(Collectors.toList()));
 		List<Float> preProcessTonesList = new ArrayList<>();
 		preProcessTonesList.addAll(preProcessLinesList.stream().map(AudioAnalysisHelper::getFloatFromLine).collect(Collectors.toList()));
 		List<Chord> chordList = new ArrayList<>();
 		List<Float> chordTimestampList = new ArrayList<>();
+		// also get chordLabel timestamps - for checking timestamps only
+		List<String> chordLabelLinesList = Files.readAllLines(new File(inputFiles.get(1)).toPath(), Charset.defaultCharset());
+		List<Float> chordLabelTimestampList = new ArrayList<>();
+		chordLabelTimestampList.addAll(chordLabelLinesList.stream().map(AudioAnalysisHelper::getTimestampFromLine).collect(Collectors.toList()));
+		// also key timestamps - for checking timestamps only
 		List<String> keyLinesList = Files.readAllLines(new File(inputFiles.get(2)).toPath(), Charset.defaultCharset());
 		List<Float> keyTimestampList = new ArrayList<>();
 		keyTimestampList.addAll(keyLinesList.stream().map(AudioAnalysisHelper::getTimestampFromLine).collect(Collectors.toList()));
@@ -86,78 +91,40 @@ public class ChordVectorsFilter extends LineChartPlugin {
 			lineIndex++;
 		}
 
-		// 1. Get timestamps from the chord labels and keys file
-		// prepares:
-		// chordLabelList (list of chord labels)
-		// chordLabelTimestampList (timestamps related to chordList)
-		List<String> chordLabelLinesList = Files.readAllLines(new File(inputFiles.get(0)).toPath(), Charset.defaultCharset());
-		List<String> chordLabelList = new ArrayList<>();
-		List<Float> chordLabelTimestampList = new ArrayList<>();
-		chordLabelList.addAll(chordLabelLinesList.stream().map(AudioAnalysisHelper::getLabelFromLine).collect(Collectors.toList()));
-		chordLabelTimestampList.addAll(chordLabelLinesList.stream().map(AudioAnalysisHelper::getTimestampFromLine).collect(Collectors.toList()));
-
-
 		// If there is no present timestamp 0.0, automatically fix: Add 0.0 timestamp with EMPTY_CHORD
 		if (chordTimestampList.get(0) > 0.000000001) {
 			chordTimestampList.add(0, 0.0f);
 			chordList.add(0, Chord.EMPTY_CHORD);
-			chordLabelTimestampList.add(0, 0.0f);
-			chordLabelList.add(0, "N");
 		}
 
 		// Check for the last timestamp - make sure that the the latest timestamp is used
 		// (check keys whether there is a later timestamp than in the chord file)
 		// Automatically fix: Add (latest timestamp) to the end of the file, with "(unknown)" key
 		float latestChordTimestamp = chordTimestampList.get(chordTimestampList.size() - 1);
+		float latestChordLabelTimestamp = chordLabelTimestampList.get(chordLabelTimestampList.size() - 1);
 		float latestKeyTimestamp = keyTimestampList.get(keyTimestampList.size() - 1);
-		if (latestKeyTimestamp > latestChordTimestamp) {
-			chordTimestampList.add(latestKeyTimestamp);
+		float latestTimestamp = (latestChordLabelTimestamp > latestKeyTimestamp) ? latestChordLabelTimestamp : latestKeyTimestamp;
+		if (latestTimestamp > latestChordTimestamp) {
+			chordTimestampList.add(latestTimestamp);
 			chordList.add(Chord.EMPTY_CHORD);
-			chordLabelTimestampList.add(latestKeyTimestamp);
-			chordLabelList.add("N");
 		}
 
-		int chordIndex = 0, chordLabelIndex = 0;
-		float chordLabelTimestamp;
+		int chordIndex = 0;
 		float chordTimestamp;
-		int keyIndex = 0;
-		keyTimestampList = new ArrayList<>();
-		String chordLabel;
 		Chord chord;
-		Chord previousChord = Chord.EMPTY_CHORD;
 
 		// 2. Iterate over both chord label and chord array - getting labels from chord label output and printing the vector
-		for (String label : chordLabelList) {
-			if ((chordLabelIndex > chordLabelTimestampList.size() - 1) || (chordLabelIndex > chordLabelList.size() - 1)) {
-				break;
-			}
+		for (int i = 0; i < chordList.size(); i++) {
+			// check whether we are still within the array
 			if ((chordIndex > chordTimestampList.size() - 1) || (chordIndex > chordList.size() - 1)) {
 				break;
 			}
-			chordLabelTimestamp = chordLabelTimestampList.get(chordLabelIndex);
+			// Get chord and its timestamp
 			chordTimestamp = chordTimestampList.get(chordIndex);
-			if (chordLabelTimestamp != chordTimestamp) {
-				chordIndex--; // do not move chordIndex this iteration
-			} else {
-				// if next key timestamp is lower than currently examined chord timestamp
-				if (((keyIndex + 1) < keyTimestampList.size()) && (keyTimestampList.get(keyIndex + 1) <= chordLabelTimestamp)) {
-					// => go to the next key timestamp
-					keyIndex++;
-				}
-
-				// Get chord
-				chord = chordList.get(chordIndex);
-
-				if ((chord == Chord.EMPTY_CHORD) || (previousChord == Chord.EMPTY_CHORD)) {
-					System.out.println("SKIP (one or both chords were not assigned)\n\n");
-				} else {
-					// Write the chord vector of boolean to the output
-					out.write(chordLabelTimestamp + ": " + chord.getStringVector() + "\n");
-				}
-				previousChord = chord;
-			}
+			chord = chordList.get(chordIndex);
+			// Write the chord vector of boolean to the output
+			out.write(chordTimestamp + ": " + chord.getStringVector() + "\n");
 			chordIndex++;
-			chordLabelIndex++;
 		}
 
 		out.close();
